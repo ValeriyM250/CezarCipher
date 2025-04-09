cb4b71d2b64469f578605fb0b7f0e83d03489577
Valeriy <valeriym250@gmail.com>
2025-04-09 14:38:18 +0100

    second commint

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,8 @@ import org.example.commonTools.CesarCipherFileCreator;
 public class Main {
     public static void main(String[] args) {
        // "c:/MyProject/target/myApp.jar DECRYPT folder/textFile1[ENCRYPTED].txt 20"
-
         CesarCipherFileCreator cesarCipherFileCreator = new CesarCipherFileCreator();
         cesarCipherFileCreator.createCesarCipherFile("c:/MyProject/target/myApp.jar enCRYPT c:/test/testNote.txt 0");
         cesarCipherFileCreator.createCesarCipherFile("c:/MyProject/target/myApp.jar deCRYPT c:/test/testNote[ENCRYPTED].txt 0");
-
     }
 }
